6774689d2b0067f93895303f0f4a0572addd9a0a
Benoit PETIT <petitbenoit3@gmail.com>
2023-11-21 13:07:45 +0100

    lerp drag with constraints

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { CSSProperties, useEffect, useState } from 'react'
+import { CSSProperties, useEffect, useRef, useState } from 'react'
 import './main.scss'
+import clsx from 'clsx'
 
-function App() {
+const lerp = (a: number, b: number, t: number) => a * (1 - t) + b * t
+
+const App = () => {
   const [windowDimensions, setWindowDimensions] = useState({
     height: window.innerHeight,
     width: window.innerWidth
   })
+  const requestRef = useRef(0)
+  const [isMouseDown, setIsMouseDown] = useState(false)
   const [dragging, setDragging] = useState(false)
-  const [dragPosition, setDragPosition] = useState({ x: 0, y: 0 })
+  const [offsetCursorPos, setOffsetCursorPos] = useState({ x: 0, y: 0 })
+  const [basePos, setBasePos] = useState({ x: 0, y: 0 })
+  const [dragPos, setDragPos] = useState({ x: 0, y: 0 })
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 })
+  const [lerpMousePos, setLerpMousePos] = useState({ x: 0, y: 0 })
 
   useEffect(() => {
     const handleResize = () => {
@@ -17,13 +26,23 @@ function App() {
       })
     }
 
+    handleResize()
+
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
   }, [])
 
   useEffect(() => {
-    const handleMouseDown = () => setDragging(true)
-    const handleMouseUp = () => setDragging(false)
+    const handleMouseDown = (e: MouseEvent) => {
+      const { x, y } = e
+      setOffsetCursorPos({ x, y })
+      setBasePos(dragPos)
+      setIsMouseDown(true)
+    }
+    const handleMouseUp = () => {
+      setIsMouseDown(false)
+      setDragging(false)
+    }
 
     window.addEventListener('mousedown', handleMouseDown)
     window.addEventListener('mouseup', handleMouseUp)
@@ -31,22 +50,61 @@ function App() {
       window.removeEventListener('mousedown', handleMouseDown)
       window.removeEventListener('mouseup', handleMouseUp)
     }
-  }, [])
+  }, [dragPos])
 
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
-      if (dragging) {
-        setDragPosition({ x: e.clientX, y: e.clientY })
+      if (isMouseDown) {
+        setDragging(true)
+      }
+      if (dragging && isMouseDown) {
+        const newPos = {
+          x: basePos.x + e.x - offsetCursorPos.x,
+          y: basePos.y + e.y - offsetCursorPos.y
+        }
+
+        setMousePos({
+          x: Math.min(
+            Math.max(-windowDimensions.width, newPos.x),
+            windowDimensions.width
+          ),
+          y: Math.min(
+            Math.max(-windowDimensions.height, newPos.y),
+            windowDimensions.height
+          )
+        })
       }
     }
 
     window.addEventListener('mousemove', handleMouseMove)
     return () => window.removeEventListener('mousemove', handleMouseMove)
-  }, [dragging])
+  }, [dragging, offsetCursorPos, basePos])
+
+  useEffect(() => {
+    setDragPos({
+      x: lerpMousePos.x,
+      y: lerpMousePos.y
+    })
+  }, [lerpMousePos])
+
+  const handleRaf = () => {
+    setLerpMousePos({
+      x: lerp(lerpMousePos.x, mousePos.x, 0.1),
+      y: lerp(lerpMousePos.y, mousePos.y, 0.1)
+    })
+
+    requestRef.current = requestAnimationFrame(handleRaf)
+  }
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(handleRaf)
+
+    return () => cancelAnimationFrame(requestRef.current)
+  }, [lerpMousePos, mousePos])
 
   return (
     <div
-      className='wrapper'
+      className={clsx('wrapper', dragging && 'is-dragging')}
       style={
         {
           '--vw': `${windowDimensions.width / 100}px`,
@@ -59,7 +117,7 @@ function App() {
           <div
             className='drag-grid'
             style={{
-              transform: `translate3d(${dragPosition.x}px, ${dragPosition.y}px, 0px)`
+              transform: `translate3d(${dragPos.x}px, ${dragPos.y}px, 0px)`
             }}
           >
             {Array.from({ length: 9 }).map((_, i) => (
@@ -68,7 +126,7 @@ function App() {
                 key={i}
                 data-row={Math.floor(i / 3)}
                 data-col={i % 3}
-                style={{ backgroundColor: `hsl(${i * 40}, 100%, 20%)` }}
+                style={{ backgroundColor: `hsl(${i * 40}, 50%, 60%)` }}
               />
             ))}
           </div>
